443afeb374ee5ea0f17aa5c39ceeab770d60c5df
utkarsh raj <utkarshraj525@gmail.com>
2025-05-28 20:12:51 +0530

    Cache & Live API

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,12 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
 
   const query = (await searchParams).query;
-  const posts = await client.fetch(STARTUPS_QUERY)
-  // console.log(JSON.stringify(posts, null, 2))
-
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY })
 
   return (
     <>
@@ -38,6 +36,9 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         </ul>
 
       </section>
+
+      {/* SanityLive */}
+      <SanityLive />
     </>
   );
 }
